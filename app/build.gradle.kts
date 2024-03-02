ce22c9b234375f5063367d59ed01aa4b6b3ea2db
Teract10s <trs.leonov@gmail.com>
2024-03-02 23:16:19 +0200

    correct to work version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.opencsv:opencsv:5.9")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.activity:activity:1.8.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
