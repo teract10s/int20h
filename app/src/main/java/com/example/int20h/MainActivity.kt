e63a45e4e5153a49ab84f211dbb47386055b014c
yuliiazhosan <131550620+yuliiazhosan@users.noreply.github.com>
2024-03-02 19:59:44 +0200

    added DeviceService

--- app/src/main/java/com/example/int20h/MainActivity.kt
+++ app/src/main/java/com/example/int20h/MainActivity.kt
@@ -2,25 +2,23 @@ package com.example.int20h
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Toast
-import com.example.int20h.repository.DeviceRepositoryListImpl
-import com.example.int20h.service.DeviceService
 
 class MainActivity : AppCompatActivity() {
-    val deviceService = DeviceService(DeviceRepositoryListImpl())
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val typeItems = deviceService.types
+        val items = listOf("1", "2", "3", "4")
         val autoComplete : AutoCompleteTextView = findViewById(R.id.auto_complete)
-        val typeAdapter = ArrayAdapter(this,R.layout.list_item,typeItems)
+        val adapter = ArrayAdapter(this,R.layout.list_item,items)
 
-        autoComplete.setAdapter(typeAdapter)
+        autoComplete.setAdapter(adapter)
         autoComplete.onItemClickListener = AdapterView.OnItemClickListener {
                 adapterView, view, i, l ->
 
