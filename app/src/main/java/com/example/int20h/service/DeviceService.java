aee9d6ff7dcfb9aa722b345daca4b3fd5a7a94de
Teract10s <trs.leonov@gmail.com>
2024-03-02 19:14:51 +0200

    updated DeviceService

--- app/src/main/java/com/example/int20h/service/DeviceService.java
+++ app/src/main/java/com/example/int20h/service/DeviceService.java
@@ -3,7 +3,9 @@ package com.example.int20h.service;
 import com.example.int20h.model.Device;
 import com.example.int20h.repository.DeviceRepository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DeviceService {
     private DeviceRepository deviceRepository;
@@ -14,5 +16,21 @@ public class DeviceService {
 
     public Optional<Device> getDeviceByTypeBrandModel(String type, String brand, String model) {
         return deviceRepository.getDeviceByTypeBrandModel(type, brand, model);
+    }
+
+    public List<String> getTypes() {
+        return deviceRepository.getTypes();
+    }
+
+    public List<String> getBrandsByType(String type) {
+        return deviceRepository.getBrandsByType(type);
+    }
+
+    public List<String> getModelsByBrand(String brand) {
+        return deviceRepository.getModelsByBrand(brand);
+    }
+
+    public boolean addAll(List<Device> devices) {
+        return deviceRepository.addAll(devices);
     }
 }
